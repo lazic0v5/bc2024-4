1b94fd19888f40cf879bda9770cec003bbc9e73a
Сорока Максим bc2024-4 <lazic0v5@gmail.com>
2024-10-18 20:44:44 +0300

    Додати перевірку на отримання картинки з http.cat

--- index.js
+++ index.js
@@ -2,6 +2,7 @@ const { Command } = require('commander');
 const http = require('http');
 const fs = require('fs').promises;
 const path = require('path');
+const superagent = require('superagent'); // Імпортуємо superagent
 
 const program = new Command();
 
@@ -29,8 +30,16 @@ const server = http.createServer(async (req, res) => {
       res.writeHead(200, { 'Content-Type': 'image/jpeg' });
       res.end(image);
     } catch (err) {
-      res.writeHead(404, { 'Content-Type': 'text/plain; charset=utf-8' }); // Встановлюємо кодування utf-8
-      res.end('Картинка не знайдена у кеші.\n');
+      // Якщо файлу немає в кеші, робимо запит на http.cat
+      try {
+        const response = await superagent.get(`https://http.cat/${statusCode}`); // Отримуємо картинку
+        await fs.writeFile(cachePath, response.body); // Зберігаємо картинку у кеш
+        res.writeHead(200, { 'Content-Type': 'image/jpeg' });
+        res.end(response.body);
+      } catch (err) {
+        res.writeHead(404, { 'Content-Type': 'text/plain; charset=utf-8' });
+        res.end('Картинка не знайдена на http.cat.\n');
+      }
     }
   } else if (req.method === 'PUT') {
     // Збереження нового файлу у кеші
@@ -58,7 +67,7 @@ const server = http.createServer(async (req, res) => {
     }
   } else {
     // Метод не підтримується
-    res.writeHead(405, { 'Content-Type': 'text/plain; charset=utf-8' });
+    res.writeHead(405, { 'Content-Type': 'text/plain' });
     res.end('Метод не дозволено.\n');
   }
 });
@@ -67,3 +76,4 @@ const server = http.createServer(async (req, res) => {
 server.listen(options.port, options.host, () => {
   console.log(`Сервер запущено на http://${options.host}:${options.port}`);
 });
+
